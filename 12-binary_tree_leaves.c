533e070e8cfe2fd1ab8c88ebcaaa1d8fbabce1ba
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-04-06 20:03:18 +0100

    12-binary_tree_leaves.c

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - computes the number of leaves
+ * 
+ * @tree: pointer to the tree
+ * 
+ * Return: number of leaves in the tree and 0 otherwise
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	if ((tree->left == NULL) && (tree->right == NULL))
+	{
+		return (1);
+	}
+
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
